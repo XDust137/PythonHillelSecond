ec2d09f71716b8f618019857c30745f67451b3ba
XDust137 <58106355+XDust137@users.noreply.github.com>
2021-07-28 14:34:50 +0300

    commit

---
+++ DZ4/car/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from car.models import Car, CarModel, Company
+
+
+class CarForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = "__all__"
+
+
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = CarModel
+        fields = "__all__"
+
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = "__all__"
