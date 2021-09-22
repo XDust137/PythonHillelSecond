0fc634e3af95427da69e57255e8185a9af3864b0
XDust137 <58106355+XDust137@users.noreply.github.com>
2021-09-22 15:52:19 +0300

    myuniverse

--- DZ3/car/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from car.views import car_view
-urlpatterns = [
-    path("car/", car_view ),
-    path("car/<int:obj>", car_view ),
-]
--- DZ6-7/universe-project/views.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
-from .serializers import ModelSerializer, GroupSerializer
-
-
